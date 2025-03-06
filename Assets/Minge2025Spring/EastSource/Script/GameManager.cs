e53ab0dc87b22ccb32681aad5436d8b58ab34414
EastSource <139376938+EastSource@users.noreply.github.com>
2025-03-06 13:53:31 +0900

    StageLayout_tentative
    取り合えず適当に土台配置した。ブランチ分けて作業する
    今日の朝ごはんはカレーと肉まん食った。おいしかった。

---
+++ Assets/Minge2025Spring/EastSource/Script/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance{get; private set;}
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
