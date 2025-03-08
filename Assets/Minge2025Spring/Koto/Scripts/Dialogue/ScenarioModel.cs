6bbc1acb106149d1fd45e45549595f2fb2d0f27f
koto-thing <gotoukenta62@gmail.com>
2025-03-09 03:23:42 +0900

    フォントアセットを作成 Jsonから文字列を取得するシステムを作成 会話システムのUIを作成

---
+++ Assets/Minge2025Spring/Koto/Scripts/Dialogue/ScenarioModel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Dialogue
+{
+    [CreateAssetMenu(fileName = "ScenarioModel", menuName = "ScriptableObject/ScenarioModel")]
+    public class ScenarioModel : ScriptableObject
+    {
+        [Header("シナリオファイルへのパス")] 
+        [SerializeField] private string[] filePath;
+        [SerializeField] private int currentFilePathIndex;
+        
+        [Header("シナリオデータ")]
+        [SerializeField] private ScenarioData scenarioData;
+        [SerializeField] private int currentSceneIndex;
+
+        /* getter と setter */
+        public string[] FilePath => filePath;
+        public int CurrentFilePathIndex { get => currentFilePathIndex; set => currentFilePathIndex = value; }
+        public ScenarioData ScenarioData { get => scenarioData; set => scenarioData = value; }
+        public int CurrentSceneIndex { get => currentSceneIndex; set => currentSceneIndex = value; }
+
+        public void Init()
+        {
+            currentSceneIndex = 0;
+            currentFilePathIndex = 0;
+        }
+    }
+}
---
+++ Assets/Minge2025Spring/Koto/Scripts/Dialogue/SenarioLoader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.IO;
+
+namespace Dialogue
+{
+    public class ScenarioLoader
+    {
+        // @brief シナリオ読み込み
+        public ScenarioData LoadScenario(string filePath)
+        {
+            ScenarioData scenarioData = null;
+            string path = Path.Combine(Application.streamingAssetsPath, filePath);
+            string jsonText = "";
+            
+            if (File.Exists(path))
+            {
+                jsonText = File.ReadAllText(path);
+                scenarioData = JsonUtility.FromJson<ScenarioData>(jsonText);
+                Debug.Log("シナリオをロードしました: " + scenarioData.scenes.Length + " 件");
+            }
+            else
+            {
+                Debug.LogError("JSONファイルが見つかりません: " + path);
+            }
+
+            return scenarioData;
+        }
+    }
+}
