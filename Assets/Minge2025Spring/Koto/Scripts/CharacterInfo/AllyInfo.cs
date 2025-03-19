d893be6868d9c1a625442d324ba5a392ec0649be
koto-thing <gotoukenta62@gmail.com>
2025-03-19 16:02:13 +0900

    バグ解消

--- Assets/Minge2025Spring/Koto/Scripts/CharacterInfo/AllyInfo.cs
+++ Assets/Minge2025Spring/Koto/Scripts/CharacterInfo/AllyInfo.cs
@@ -14,13 +14,13 @@ namespace CharacterInfo
         [SerializeField] private int attack;
         [SerializeField] private int defence;
         
-        public AttackType AttackType  { get => attackType; set => attackType = value; }
+        public AttackType AttackType    { get => attackType; set => attackType = value; }
         public LayerMask CharacterLayer { get => characterLayer; }
-        public Sprite CharacterSprite { get => characterSprite; set => characterSprite = value; }
-        public string CharacterName   { get => characterName; set => characterName = value; }
-        public int Cost               { get => cost; set => cost = value; }
-        public int Hp                 { get => hp; set => hp = value; }
-        public int Attack             { get => attack; set => attack = value; }
-        public int Defence            { get => defence; set => defence = value; }
+        public Sprite CharacterSprite   { get => characterSprite; set => characterSprite = value; }
+        public string CharacterName     { get => characterName; set => characterName = value; }
+        public int Cost                 { get => cost; set => cost = value; }
+        public int Hp                   { get => hp; set => hp = value; }
+        public int Attack               { get => attack; set => attack = value; }
+        public int Defence              { get => defence; set => defence = value; }
     }
 }
