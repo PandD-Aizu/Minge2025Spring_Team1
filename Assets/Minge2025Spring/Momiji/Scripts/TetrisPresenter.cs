5bb6a699948c31b6679a7e9792b7c6d8fd7d2db9
komamomiji <yokohirahazuki0607@gmail.com>
2025-03-24 00:37:43 +0900

    パズルの作成
    ブロックを落として穴に入れるパズルを作成しました。

---
+++ Assets/Minge2025Spring/Momiji/Scripts/TetrisPresenter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace Puzzle
+{
+    // サンプルのパズル
+    //　viewとmodelのデータのやり取りを仲介をするクラス
+    public class TetrisPresenter : MonoBehaviour, IDisposable
+    {
+        [Header("依存関係")]
+        [SerializeField] private TetrisModel model;
+        [SerializeField] private TetrisView view;
+        [SerializeField] private BlockScript block;
+
+        // @brief エントリポイント
+        private void Start()
+        {
+            view.Initialize();
+            SubScribeEvents();
+        }
+        
+        // @brief エントリポイント
+        private void Update()
+        {
+            view.AttractParticle();
+        }
+
+        // @brief イベント群の登録
+        private void SubScribeEvents()
+        {
+            // 落とすボタンが押されたときのイベントを登録
+            view.PushButton.onClick.AddListener(() =>
+            {
+                view.ScalingCookie();                        // ボタンの拡縮アニメーション
+                block.rb.useGravity = true;
+                block.flag = false;
+            });
+        }
+
+        public void ClearGame()
+        {
+            block.rb.useGravity = false;
+            //view.PlayParticle();                         // パーティクルの再生
+            model.IsSolved.SetValueAndForceNotify(true); // パズルを解いたことを通知
+        }
+
+        // @brief メモリリークを防ぐための処理
+        public void Dispose()
+        {
+            
+        }
+    }
+}
