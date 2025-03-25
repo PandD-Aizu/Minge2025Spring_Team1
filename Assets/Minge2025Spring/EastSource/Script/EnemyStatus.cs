0c7dd0f915abda5753a25eaf5c41e2929e30d906
EastSource <139376938+EastSource@users.noreply.github.com>
2025-03-25 12:48:21 +0900

    SettingSpawnLimiter
    GameSpawnManagerにフィールド上で一度にスポーン出来るエネミーの数を設定できるようにしました。
    
    天井の研ナオコが気になって眠れない！！！！！！！！！！！！！！！！！！！

--- Assets/Minge2025Spring/EastSource/Script/EnemyStatus.cs
+++ Assets/Minge2025Spring/EastSource/Script/EnemyStatus.cs
@@ -58,7 +58,7 @@ public class EnemyStatus : MonoBehaviour
 
     private void OnDestroy()
     {
-        
+        GameSpawnManager.Instance.EnemyDestroy();
     }
 
     public void TakeDamage(int damage)
