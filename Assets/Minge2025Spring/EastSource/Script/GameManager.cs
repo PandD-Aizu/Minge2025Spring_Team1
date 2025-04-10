a223cf76598717f71ad7a118ef35aa449a6ce4c6
EastSource <139376938+EastSource@users.noreply.github.com>
2025-04-10 16:58:55 +0900

    aa

--- Assets/Minge2025Spring/EastSource/Script/GameManager.cs
+++ Assets/Minge2025Spring/EastSource/Script/GameManager.cs
@@ -374,4 +374,14 @@ public class GameManager : MonoBehaviour
             OnGameOver?.Invoke(this, EventArgs.Empty);
         }
     }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
 }
