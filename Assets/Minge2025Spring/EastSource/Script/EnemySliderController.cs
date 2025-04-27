4d5dbce88e2593de419251628b21851ad5444d0c
koto-thing <gotoukenta62@gmail.com>
2025-04-27 15:39:30 +0900

    HPSliderを描画

---
+++ Assets/Minge2025Spring/EastSource/Script/EnemySliderController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Minge2025Spring.EastSource.Script
+{
+    public class EnemySliderController : MonoBehaviour
+    {
+        [Header("依存関係")]
+        [SerializeField] private EnemyStatus enemyStatus;
+        
+        [Header("スライダー")]
+        [SerializeField] private Slider hpSlider; // HPスライダー
+
+        private void Start()
+        {
+            hpSlider.maxValue = enemyStatus.MaxHealth;
+        }
+
+        private void Update()
+        {
+            ChangeSliderValue();
+        }
+
+        // @brief スライダーの値を変更する
+        private void ChangeSliderValue()
+        {
+            hpSlider.value = enemyStatus.CurrentHealth;
+        }
+    }
+}
