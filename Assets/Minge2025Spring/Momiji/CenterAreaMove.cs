033e58bc0845dea6a89f0c5bf9d1877deba618f1
komamomiji <yokohirahazuki0607@gmail.com>
2025-05-09 20:19:34 +0900

    ステージ２、床の中央が動くギミックの作成
    ３０秒ごとにステージ中央の床が上下に動き、下がっている時は敵が歩ける床、上がっている時は歩けない床になります。

--- Assets/Minge2025Spring/Momiji/CenterAreaMove.cs
+++ Assets/Minge2025Spring/Momiji/CenterAreaMove.cs
@@ -3,68 +3,49 @@ using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 
-public class CenterAreaMove : MonoBehaviour
-{
-    [SerializeField] private GameObject CenterEnemyWalk;
+public class CenterAreaMove : MonoBehaviour{
     [SerializeField] private GameObject CenterCannotWalk;
-    private float timer;
-    private float movingtime = 10f;
-    private float speed = 0.5f;
-
-    private Transform centerpos;
-    private bool flag;
+    private float timer;　//時間計算用変数
+    private float movingtime = 30f; //床が動くクールタイム
+    private float speed = 0.1f;　//床が動くスピード
+    private bool flag; //床が上か下かの判定
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         timer = 0;
-        flag = true;
+        flag = false;
     }
 
     // Update is called once per frame
     public async void Update()
     {
         timer += Time.deltaTime;
-
-        if (timer >= movingtime)
+        
+        if (CenterCannotWalk.transform.position.y > -2.75 && flag==true)
         {
-            await MovingArea();
-            if (flag == true)
+            CenterCannotWalk.transform.position += new Vector3(0, -speed, 0); //床を下げる
+        }
+        else if (CenterCannotWalk.transform.position.y < -0.79 && flag==false)
+        {
+            CenterCannotWalk.transform.position += new Vector3(0, speed, 0); //床を上げる
+        }
+
+        if (timer >= movingtime)　//一定時間経過後に
+        {
+            if (flag == true)　//床が下がっていたら
             {
-                centerpos = CenterCannotWalk.transform;
-                //Instantiate(CenterEnemyWalk, centerpos);
-                //DestroyImmediate(CenterCannotWalk,true);
                 timer = 0;
                 flag = false;
+                CenterCannotWalk.layer = 10;
+                CenterCannotWalk.tag = "EnemyUnWalkable";
             }
-            else
+            else //床が上がっていたら
             {
-                centerpos = CenterEnemyWalk.transform;
-                //Instantiate(CenterCannotWalk, centerpos);
-                //DestroyImmediate(CenterEnemyWalk,true);
                 timer = 0;
                 flag = true;
+                CenterCannotWalk.layer = 9;
+                CenterCannotWalk.tag = "EnemyWalkable";
             }
-        }
-    }
-
-    public async UniTask MovingArea()
-    {
-        if (flag)
-        {
-            while (CenterCannotWalk.transform.position.y > -2)
-            {
-                await UniTask.Delay(1000);
-                CenterCannotWalk.transform.position += new Vector3(0, -speed, 0);
-            }
-        }
-        else
-        {
-            while (CenterCannotWalk.transform.position.y < 0)
-            {
-                await UniTask.Delay(1000); 
-                CenterCannotWalk.transform.position += new Vector3(0, speed, 0);
-            }
-            
         }
     }
 }
