a455c913b62b58beff53d29729a4dfdd7bb0ff3a
momiji1107 <yokohirahazuki0607@gmail.com>
2025-06-23 20:04:15 +0900

    微調整しました。

--- Assets/Minge2025Spring/Momiji/Scripts/ExpManager.cs
+++ Assets/Minge2025Spring/Momiji/Scripts/ExpManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using CharacterInfo;
 
 public class ExpManager : MonoBehaviour
 {
     public int playerExp;
-    public ScriptableObject[] characters;
+    private int addExp = 10;
+    private int upAttack = 50;
+    private int level = 0;
+    public AllyInfo[] characters;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,21 +17,25 @@ public class ExpManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(playerExp >= 30)
-        {
-            for (int i = 0; i < 4; i++) ; //characters[i].Attack += 50;
-        }
+        if (playerExp >= 40 && level == 0) levelUp();
+        if (playerExp >= 90 && level == 1) levelUp();
+    }
+
+    void levelUp()
+    {
+        for (int i = 0; i < 4; i++) characters[i].Attack += upAttack;
+        level++;
     }
 
     public void AddExp(GameObject enemy)
     {
         if (Equals(enemy.gameObject.name, "_BaseEnemy"))
         {
-            playerExp += 10;
+            playerExp += addExp;
         }
         else if (Equals(enemy.gameObject.name, "FlyEnemy"))
         {
-            playerExp += 10;
+            playerExp += addExp;
         }
     }
 }
