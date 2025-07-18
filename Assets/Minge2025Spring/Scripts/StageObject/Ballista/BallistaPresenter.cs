2d2a01a19737c6ee7455fadf7461e3a702ce0e26
koto-thing <gotoukenta62@gmail.com>
2025-07-18 20:16:09 +0900

    といあえず、push(デバイス変更)

---
+++ Assets/Minge2025Spring/Scripts/StageObject/Ballista/BallistaPresenter.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+namespace StageObject
+{
+    public class BallistaPresenter : MonoBehaviour
+    {
+        [SerializeField] private BallistaModel model;
+        [SerializeField] private BallistaView view;
+
+        public void Update()
+        {
+            if (model.TargetEnemy != null && model.CurrentCoolDown >= model.AttackCoolDown)
+            {
+                // クールタイムをリセット
+                model.CurrentCoolDown = 0.0f;
+                
+                view.RotateToEnemy(model.TargetEnemy.transform);
+                
+                model.TargetEnemy.GetComponent<EnemyStatus>()?.TakeDamage(150); // ターゲットの敵にダメージを与える
+            }
+            
+            // クールダウンの更新
+            model.CurrentCoolDown += Time.deltaTime;
+        }
+
+        public void OnTriggerStay(Collider other)
+        {
+            Debug.Log("OnTriggerStay");
+            
+            if (other.gameObject.CompareTag("Enemy") && model.TargetEnemy == null)
+            {
+                model.TargetEnemy = other.gameObject;
+            }
+        }
+        
+        public void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Enemy") && model.TargetEnemy == other.gameObject)
+            {
+                model.TargetEnemy = null;
+            }
+        }
+    }
+
+}
---
+++ Assets/Minge2025Spring/Scripts/StageObject/Ballista/BallistaView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace StageObject
+{
+    public class BallistaView : MonoBehaviour
+    {
+        [SerializeField] private GameObject ballistaObject;
+
+        /// <summary>
+        /// Ballistaの向きをターゲットの方向に向ける
+        /// </summary>
+        /// <param name="target">敵のTransform</param>>
+        public void RotateToEnemy(Transform target)
+        {
+            Vector3 direction = target.position - ballistaObject.transform.position;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            ballistaObject.transform.rotation = Quaternion.Slerp(ballistaObject.transform.rotation, rotation, Time.deltaTime * 2f);
+        }
+    }
+}
